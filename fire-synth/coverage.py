79c9168909b0a43e8b9fa99efee83b5fa00fb605
BobYXLee <bobyxlee@gmail.com>
2024-03-24 20:58:10 +0800

    Created script to generate synthetic data for coverage check

---
+++ fire-synth/coverage.py
@@ -0,0 +1,53 @@
+"""
+coverage.py
+
+Synthetic dataset generator for coverage checks
+
+"""
+import sys
+import os
+import random
+import pprint
+from copy import deepcopy
+from uuid import uuid4
+import json
+
+sys.path.append("../fire-opt/packages")
+
+from geom.Line2D import Line2D, drawPt
+from geom import Poly2D, Bool2D
+from geom.Room import Room
+
+from datetime import date
+from time import time
+import numpy as np
+import pyclipper
+
+out_dict = {"date_generated": str(date.today()) ,"data":[]}
+
+def gData():
+    room = Room.random(2)
+    room.gExtSlots(1000)
+    pick_num = random.randint(1,3)
+    exts = random.sample(room.ext_slots, pick_num)
+    exts_clean = []
+    for e in exts:
+        exts_clean.append(e.tolist())
+    chk = room.extCoverChk(exts)
+    data = {"id": str(uuid4()), "room": room.vertices, 
+            "extinguishers": exts_clean, "comply": int(chk["result"])}
+    return data
+
+if __name__ == "__main__":
+    for i in range(0,1000):
+        out_dict["data"].append(gData())
+        print("Data point %d created." % i)
+        pass
+    out_dir = os.path.join(os.getcwd(), "dataset")
+    os.makedirs(out_dir, exist_ok = True)
+    filename = "%s_%s.json" % (str(int(time())), str(uuid4()))
+    filepath =os.path.join(out_dir, filename)
+    print("Writing dataset to %s..." % filepath)
+    with open(filepath, 'w') as f:
+        f.write(json.dumps(out_dict))
+    print("Dataset Generation completed.")
